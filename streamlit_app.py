47d3c7bc77fd1a4986e7a8e339e6c1d9f2c06e54
Shawn Crivello <crivello.shawn@gmail.com>
2023-06-17 19:48:19 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ def load_data():
 
     return techniques_df
 
-@st.cache_data
+@st.cache_data(ttl=3600)
 def fetch_and_cache_data():
     return load_data()
 
@@ -36,8 +36,30 @@ def main():
     if selected_platform != 'All':
         data = data[data['x_mitre_platforms'].apply(lambda x: selected_platform in x if x else False)]
 
+    tactics = list(set([item for sublist in data['kill_chain_phases'].dropna() for item in sublist]))
+    tactics.sort()
+    selected_tactic = st.selectbox("Select a Tactic", ['All'] + tactics)
+
+    if selected_tactic != 'All':
+        data = data[data['kill_chain_phases'].apply(lambda x: selected_tactic in x if x else False)]
+
+    techniques = list(set(data['name']))
+    techniques.sort()
+    selected_technique = st.selectbox("Select a Technique", ['All'] + techniques)
+
+    if selected_technique != 'All':
+        data = data[data['name'] == selected_technique]
+
+    # Display filtered data
     if st.checkbox('Show raw data'):
         st.write(data)
+
+    # Supplemental Information
+    if st.checkbox('Show Supplemental Information'):
+        technique_data = data[data['name'] == selected_technique]
+        st.write("Description:", technique_data['description'].values[0])
+        st.write("Detection:", technique_data['x_mitre_detection'].values[0])
+        st.write("Mitigation:", technique_data['x_mitre_mitigations'].values[0])
 
     # Visualization
     st.altair_chart(
