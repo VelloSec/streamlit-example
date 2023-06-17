14e65d09f4490ef8e2d3927a953d349a148fe702
Shawn Crivello <crivello.shawn@gmail.com>
2023-06-17 19:44:46 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,45 +1,56 @@
-import streamlit as st
+import json
 import pandas as pd
-from taxii2client.v20 import Server
-from stix2 import TAXIICollectionSource, Filter
+import requests
+import streamlit as st
+import altair as alt
 
-@st.cache(allow_output_mutation=True)
 def load_data():
-    # Establish TAXII2 Collection instance for Enterprise ATT&CK collection
-    server = Server("https://cti-taxii.mitre.org/taxii/")
-    api_root = server.api_roots[0]
-    collection = api_root.collections[0]
-    src = TAXIICollectionSource(collection)
+    url = "https://raw.githubusercontent.com/mitre/cti/master/enterprise-attack/enterprise-attack.json"
+    response = requests.get(url)
+    raw_data = response.json()
+    
+    techniques = []
+    for obj in raw_data["objects"]:
+        if obj['type'] == 'attack-pattern':
+            techniques.append(obj)
+            
+    techniques_df = pd.json_normalize(techniques)
 
-    # Define Filters to retrieve only the required objects
-    filter_objs = {"intrusion-set", "malware", "tool"}
+    return techniques_df
 
-    attack_data = []
-    for obj_type in filter_objs:
-        f = Filter("type", "=", obj_type)
-        attack_data.extend(src.query([f]))
-
-    return attack_data
+@st.cache_data
+def fetch_and_cache_data():
+    return load_data()
 
 def main():
     st.title("MITRE ATT&CK Navigator")
 
-    # Load STIX data
-    attack_data = load_data()
+    # Load data
+    data = fetch_and_cache_data()
 
-    # Create a DataFrame for better visualization
-    attack_df = pd.DataFrame([item for item in attack_data])
-    attack_df = attack_df[['type', 'name', 'description', 'aliases', 'created', 'modified']]
+    # Filter options
+    platforms = list(set([item for sublist in data['x_mitre_platforms'].dropna() for item in sublist]))
+    platforms.sort()
+    selected_platform = st.selectbox("Select a platform", ['All'] + platforms)
 
-    # Display the DataFrame
+    if selected_platform != 'All':
+        data = data[data['x_mitre_platforms'].apply(lambda x: selected_platform in x if x else False)]
+
     if st.checkbox('Show raw data'):
-        st.write(attack_df)
+        st.write(data)
 
-    # Show specific types of data based on user's selection
-    attack_types = attack_df['type'].unique().tolist()
-    attack_types.sort()
-    selected_type = st.selectbox("Select a type", attack_types)
-    st.write(attack_df[attack_df['type'] == selected_type])
+    # Visualization
+    st.altair_chart(
+        alt.Chart(data)
+            .mark_circle(size=60)
+            .encode(
+                x='kill_chain_phases:O',
+                y='x_mitre_platforms:O',
+                color='kill_chain_phases:N',
+                tooltip=['name', 'description', 'kill_chain_phases', 'x_mitre_platforms']
+            ).interactive(),
+        use_container_width=True
+    )
 
 if __name__ == "__main__":
     main()
