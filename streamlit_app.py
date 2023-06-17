412fc9979dba94088959ad980c4eaaa3cc0ebdfb
Shawn Crivello <crivello.shawn@gmail.com>
2023-06-17 19:38:55 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,26 @@
+
 import requests
-import os
-from collections import Counter
+import json
 from stix2 import parse
 import pandas as pd
 import matplotlib.pyplot as plt
 import streamlit as st
+import openai
 
 # URL to the STIX data file in your GitHub repo
 url = 'https://raw.githubusercontent.com/mitre-attack/attack-stix-data/master/enterprise-attack/enterprise-attack.json'
 
-@st.cache
 def load_data():
     # Fetch the STIX data file
     response = requests.get(url)
     response.raise_for_status()  # Raise exception if the request failed
 
     # Parse the STIX data
-    bundle = parse(response.text)
+    bundle_data = json.loads(response.text)
+    if 'spec_version' in bundle_data:
+        del bundle_data['spec_version']  # Remove the 'spec_version' field
+
+    bundle = parse(bundle_data)
 
     # Extract entities from the bundle
     techniques = [obj for obj in bundle.objects if obj['type'] == 'attack-pattern']
@@ -26,68 +30,41 @@ def load_data():
 
     return techniques, actors, software, relationships
 
-def generate_response(question, context, api_key):
-    # Use the OpenAI API to generate a response to the question based on the context
-    import openai
-    openai.api_key = api_key
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=f"{context}\n{question}\nAnswer:",
-        temperature=0.5,
-        max_tokens=100
-    )
-    return response.choices[0].text.strip()
-
 def main():
     st.title("MITRE ATT&CK Navigator")
 
-    # User input for OpenAI API key
-    api_key = st.text_input("Enter your OpenAI API Key", type="password")
-
-    # Load data
+    # Load STIX data
     techniques, actors, software, relationships = load_data()
 
-    # Visualization of the top 5 most common techniques used by actors
-    actor_to_techs = [rel for rel in relationships if rel['source_ref'] in [actor['id'] for actor in actors]]
-    technique_counts = Counter([rel['target_ref'] for rel in actor_to_techs])
-    top_techniques = technique_counts.most_common(5)
-    fig, ax = plt.subplots()
-    ax.bar([tech[0] for tech in top_techniques], [tech[1] for tech in top_techniques])
-    ax.set_xlabel('Technique')
-    ax.set_ylabel('Count')
-    ax.set_title('Top 5 most common techniques used by actors')
-    st.pyplot(fig)
+    # Display data in a table
+    if st.checkbox('Show raw data'):
+        st.subheader('Techniques')
+        st.write(techniques)
+        st.subheader('Actors')
+        st.write(actors)
+        st.subheader('Software')
+        st.write(software)
 
-    # Search functionality
-    query = st.text_input("Search for techniques, actors, or software")
-    if query:
-        st.subheader("Techniques")
-        for technique in [t for t in techniques if query.lower() in t['name'].lower()]:
-            st.write(technique['name'])
+    # Select a specific technique
+    selected_technique = st.selectbox("Select a technique", techniques)
+    st.write("You selected: ", selected_technique)
 
-        st.subheader("Actors")
-        for actor in [a for a in actors if query.lower() in a['name'].lower()]:
-            st.write(actor['name'])
-
-        st.subheader("Software")
-        for soft in [s for s in software if query.lower() in s['name'].lower()]:
-            st.write(soft['name'])
-
-    # The currently viewed entities - replace this with the actual data based on your application's state
-    current_entities = []  
-
-    # GPT-3 Integration
-    use_gpt = st.checkbox('Enable GPT-3', value=False)
-    if use_gpt and api_key:
-        question = st.text_input("Ask a question")
-        if question:
-            # Create the context based on the current view
-            context = "\n".join([f"{entity['name']}: {entity['description']}" for entity in current_entities])
-
-            # Generate the answer based on the context
-            answer = generate_response(question, context, api_key)
-            st.write(f"Answer: {answer}")
+    # Chat with GPT-3 about the selected technique
+    chatGPT_api_key = st.text_input("Enter your OpenAI GPT-3 API Key", type="password")
+    if st.button("Chat about the selected technique"):
+        if not chatGPT_api_key:
+            st.write("Please enter your OpenAI GPT-3 API Key")
+        else:
+            openai.api_key = chatGPT_api_key
+            response = openai.Completion.create(
+                engine="text-davinci-002",
+                prompt=f"I want to learn about {selected_technique}",
+                temperature=0.5,
+                max_tokens=100
+            )
+            st.write(response.choices[0].text.strip())
 
 if __name__ == "__main__":
     main()
+
 
