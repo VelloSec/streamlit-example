92b3717d6d35f1883fce99a1f5ec43082dd5c646
Shawn Crivello <crivello.shawn@gmail.com>
2023-06-17 19:41:55 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,70 +1,45 @@
-
-import requests
-import json
-from stix2 import parse
-import pandas as pd
-import matplotlib.pyplot as plt
 import streamlit as st
-import openai
+import pandas as pd
+from taxii2client.v20 import Server
+from stix2 import TAXIICollectionSource, Filter
 
-# URL to the STIX data file in your GitHub repo
-url = 'https://raw.githubusercontent.com/mitre-attack/attack-stix-data/master/enterprise-attack/enterprise-attack.json'
-
+@st.cache(allow_output_mutation=True)
 def load_data():
-    # Fetch the STIX data file
-    response = requests.get(url)
-    response.raise_for_status()  # Raise exception if the request failed
+    # Establish TAXII2 Collection instance for Enterprise ATT&CK collection
+    server = Server("https://cti-taxii.mitre.org/taxii/")
+    api_root = server.api_roots[0]
+    collection = api_root.collections[0]
+    src = TAXIICollectionSource(collection)
 
-    # Parse the STIX data
-    bundle_data = json.loads(response.text)
-    if 'spec_version' in bundle_data:
-        del bundle_data['spec_version']  # Remove the 'spec_version' field
+    # Define Filters to retrieve only the required objects
+    filter_objs = {"intrusion-set", "malware", "tool"}
 
-    bundle = parse(bundle_data)
+    attack_data = []
+    for obj_type in filter_objs:
+        f = Filter("type", "=", obj_type)
+        attack_data.extend(src.query([f]))
 
-    # Extract entities from the bundle
-    techniques = [obj for obj in bundle.objects if obj['type'] == 'attack-pattern']
-    actors = [obj for obj in bundle.objects if obj['type'] == 'intrusion-set']
-    software = [obj for obj in bundle.objects if obj['type'] == 'malware']
-    relationships = [obj for obj in bundle.objects if obj['type'] == 'relationship']
-
-    return techniques, actors, software, relationships
+    return attack_data
 
 def main():
     st.title("MITRE ATT&CK Navigator")
 
     # Load STIX data
-    techniques, actors, software, relationships = load_data()
+    attack_data = load_data()
 
-    # Display data in a table
+    # Create a DataFrame for better visualization
+    attack_df = pd.DataFrame([item for item in attack_data])
+    attack_df = attack_df[['type', 'name', 'description', 'aliases', 'created', 'modified']]
+
+    # Display the DataFrame
     if st.checkbox('Show raw data'):
-        st.subheader('Techniques')
-        st.write(techniques)
-        st.subheader('Actors')
-        st.write(actors)
-        st.subheader('Software')
-        st.write(software)
+        st.write(attack_df)
 
-    # Select a specific technique
-    selected_technique = st.selectbox("Select a technique", techniques)
-    st.write("You selected: ", selected_technique)
-
-    # Chat with GPT-3 about the selected technique
-    chatGPT_api_key = st.text_input("Enter your OpenAI GPT-3 API Key", type="password")
-    if st.button("Chat about the selected technique"):
-        if not chatGPT_api_key:
-            st.write("Please enter your OpenAI GPT-3 API Key")
-        else:
-            openai.api_key = chatGPT_api_key
-            response = openai.Completion.create(
-                engine="text-davinci-002",
-                prompt=f"I want to learn about {selected_technique}",
-                temperature=0.5,
-                max_tokens=100
-            )
-            st.write(response.choices[0].text.strip())
+    # Show specific types of data based on user's selection
+    attack_types = attack_df['type'].unique().tolist()
+    attack_types.sort()
+    selected_type = st.selectbox("Select a type", attack_types)
+    st.write(attack_df[attack_df['type'] == selected_type])
 
 if __name__ == "__main__":
     main()
-
-
