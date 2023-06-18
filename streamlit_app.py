7601711934cf15ae9bd78fe875f1e9e052ab1093
Shawn Crivello <crivello.shawn@gmail.com>
2023-06-17 20:17:20 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,38 +15,31 @@ def process_data(data):
     df = pd.json_normalize(attack_patterns)
 
     software = sorted(list(set(software for sublist in df['x_mitre_platforms'].dropna() for software in sublist)))
-    data_sources = sorted(list(set(source for sublist in df['x_mitre_data_sources'].dropna() for source in sublist)))
+    tactics = sorted(list(set(tactic for sublist in df['kill_chain_phases'].dropna() for phase in sublist for tactic in phase['kill_chain_name'])))
 
-    groups = []
-    if 'x_mitre_groups' in df:
-        groups = sorted(list(set(group for sublist in df['x_mitre_groups'].dropna() for group in sublist)))
+    return df, software, tactics
 
-    return df, software, groups, data_sources
-
-def apply_filters(df, software, groups, data_sources, search_term):
+def apply_filters(df, software, tactics, search_term):
     if software:
         df = df[df['x_mitre_platforms'].apply(lambda x: bool(set(software) & set(x if x else [])))]
-    if groups:
-        df = df[df['x_mitre_groups'].apply(lambda x: bool(set(groups) & set(x if x else [])))]
-    if data_sources:
-        df = df[df['x_mitre_data_sources'].apply(lambda x: bool(set(data_sources) & set(x if x else [])))]
+    if tactics:
+        df = df[df['kill_chain_phases'].apply(lambda x: bool(set(tactics) & set(phase['kill_chain_name'] for phase in x if phase.get('kill_chain_name'))))]
     if search_term:
-        df = df[df['name'].str.contains(search_term, case=False)]
+        df = df[df['name'].str.contains(search_term, case=False) | df['description'].str.contains(search_term, case=False)]
     return df
 
 def main():
     st.title('Enterprise ATT&CK Matrix Explorer')
 
     data = load_data()
-    df, software, groups, data_sources = process_data(data)
+    df, software, tactics = process_data(data)
 
     st.sidebar.header('Filters')
     software_filter = st.sidebar.multiselect('Software', software)
-    groups_filter = st.sidebar.multiselect('Groups', groups)
-    data_source_filter = st.sidebar.multiselect('Data Source', data_sources)
-    search_term = st.sidebar.text_input('Search Techniques')
+    tactics_filter = st.sidebar.multiselect('Tactics', tactics)
+    search_term = st.sidebar.text_input('Search TTPs')
 
-    filtered_df = apply_filters(df, software_filter, groups_filter, data_source_filter, search_term)
+    filtered_df = apply_filters(df, software_filter, tactics_filter, search_term)
 
     if not filtered_df.empty:
         st.dataframe(filtered_df[['name', 'description']])
