29eb0a8ecfca8091e4824ed7d1c1531408660c59
Shawn Crivello <crivello.shawn@gmail.com>
2023-06-17 21:05:46 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,12 @@
 import streamlit as st
 import pandas as pd
 
-# Load the data from the GitHub repository
-@st.cache_data(allow_output_mutation=True)
+@st.cache(allow_output_mutation=True)
 def load_data():
     url = 'https://raw.githubusercontent.com/mitre/cti/master/enterprise-attack/enterprise-attack.json'
     data = pd.read_json(url)
     return data
 
-# Process the data and extract relevant information
 def process_data(data):
     techniques = data['objects']
     software = sorted(list(set(technique.get('x_mitre_products', []) for technique in techniques)))
@@ -21,35 +19,24 @@ def process_data(data):
 def main():
     st.title("MITRE ATT&CK Navigator Data Analysis")
 
-    # Load the data
     data = load_data()
-
-    # Process the data
     techniques, software, tactics, groups, data_sources = process_data(data)
 
-    # Sidebar filters
-    st.sidebar.header("Filters")
-    selected_software = st.sidebar.selectbox("Software", software)
-    selected_tactic = st.sidebar.selectbox("Tactic", tactics)
-    selected_group = st.sidebar.selectbox("APT Group", groups)
-    selected_data_source = st.sidebar.selectbox("Data Source", data_sources)
+    selected_software = st.selectbox("Software", software)
+    selected_tactic = st.selectbox("Tactic", tactics)
+    selected_group = st.selectbox("APT Group", groups)
+    selected_data_source = st.selectbox("Data Source", data_sources)
 
-    # Filter the techniques based on selected filters
     filtered_techniques = techniques
     if selected_software:
-        filtered_techniques = [technique for technique in filtered_techniques if
-                               selected_software in technique.get('x_mitre_products', [])]
+        filtered_techniques = [technique for technique in filtered_techniques if selected_software in technique.get('x_mitre_products', [])]
     if selected_tactic:
-        filtered_techniques = [technique for technique in filtered_techniques if
-                               selected_tactic in technique.get('x_mitre_tactics', [])]
+        filtered_techniques = [technique for technique in filtered_techniques if selected_tactic in technique.get('x_mitre_tactics', [])]
     if selected_group:
-        filtered_techniques = [technique for technique in filtered_techniques if
-                               selected_group in technique.get('x_mitre_groups', [])]
+        filtered_techniques = [technique for technique in filtered_techniques if selected_group in technique.get('x_mitre_groups', [])]
     if selected_data_source:
-        filtered_techniques = [technique for technique in filtered_techniques if
-                               selected_data_source in technique.get('x_mitre_data_sources', [])]
+        filtered_techniques = [technique for technique in filtered_techniques if selected_data_source in technique.get('x_mitre_data_sources', [])]
 
-    # Show supplemental/related information for selected techniques
     if filtered_techniques:
         st.header("Selected Techniques:")
         for technique in filtered_techniques:
@@ -60,16 +47,12 @@ def main():
             st.write("APT Group:", technique.get('x_mitre_groups', []))
             st.write("---")
 
-    # Show additional analytics based on filters
-    if filtered_techniques:
         st.header("Analytics:")
-        tactic_counts = pd.DataFrame([(technique['x_mitre_tactics'], 1) for technique in filtered_techniques],
-                                     columns=['Tactic', 'Count'])
+        tactic_counts = pd.DataFrame([(technique['x_mitre_tactics'], 1) for technique in filtered_techniques], columns=['Tactic', 'Count'])
         st.subheader("Tactic Counts")
         st.dataframe(tactic_counts)
 
-        software_counts = pd.DataFrame([(technique['x_mitre_products'], 1) for technique in filtered_techniques],
-                                       columns=['Software', 'Count'])
+        software_counts = pd.DataFrame([(technique['x_mitre_products'], 1) for technique in filtered_techniques], columns=['Software', 'Count'])
         st.subheader("Software Counts")
         st.dataframe(software_counts)
 
