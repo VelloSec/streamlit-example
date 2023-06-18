d966574d3eeee2a97322c59d5b656b574ed73db0
Shawn Crivello <crivello.shawn@gmail.com>
2023-06-17 20:28:25 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,56 +1,66 @@
 import streamlit as st
 import pandas as pd
+import requests
 
 def load_data():
-    data = pd.read_json('enterprise-attack.json')
+    url = "https://raw.githubusercontent.com/mitre/cti/master/enterprise-attack/enterprise-attack.json"
+    response = requests.get(url)
+    data = response.json()
     return data
 
-def filter_data(data, software, tactics, techniques, groups, data_sources, initial_access):
-    df = data.copy()
-    
-    if software:
-        df = df[df['x_mitre_products'].apply(lambda x: any(item for item in x if software.lower() in item.lower()))]
-    if tactics:
-        df = df[df['kill_chain_phases'].apply(lambda x: any(item for item in x if tactics.lower() in item['phase_name'].lower()))]
-    if techniques:
-        df = df[df['name'].apply(lambda x: techniques.lower() in x.lower())]
-    if groups:
-        df = df[df['x_mitre_groups'].apply(lambda x: any(item for item in x if groups.lower() in item.lower()))]
-    if data_sources:
-        df = df[df['x_mitre_data_sources'].apply(lambda x: any(item for item in x if data_sources.lower() in item.lower()))]
-    if initial_access:
-        df = df[df['x_mitre_initial_access'].apply(lambda x: any(item for item in x if initial_access.lower() in item.lower()))]
-    
-    return df
-
 def process_data(data):
-    software = sorted(list(set(item for sublist in data['x_mitre_products'].dropna() for item in sublist)))
-    tactics = sorted(list(set(item['phase_name'] for sublist in data['kill_chain_phases'].dropna() for item in sublist)))
-    techniques = sorted(list(set(data['name'])))
-    groups = sorted(list(set(item for sublist in data['x_mitre_groups'].dropna() for item in sublist)))
-    data_sources = sorted(list(set(item for sublist in data['x_mitre_data_sources'].dropna() for item in sublist)))
-    initial_access = sorted(list(set(item for sublist in data['x_mitre_initial_access'].dropna() for item in sublist)))
+    objects = data.get("objects", [])
+    df = pd.DataFrame(objects)
+
+    software = sorted(list(set(df["x_mitre_products"].dropna())))
+    tactics = sorted(list(set(df["kill_chain_phases"].dropna())))
+    techniques = sorted(list(set(df["name"])))
+    groups = sorted(list(set(group for sublist in df["x_mitre_groups"].dropna() for group in sublist)))
+
+    return df, software, tactics, techniques, groups
+
+def filter_data(df, software_filter, tactics_filter, technique_filter, group_filter):
+    filtered_df = df.copy()
     
-    return software, tactics, techniques, groups, data_sources, initial_access
+    if software_filter:
+        filtered_df = filtered_df[filtered_df["x_mitre_products"].apply(lambda x: software_filter in x)]
+    
+    if tactics_filter:
+        filtered_df = filtered_df[filtered_df["kill_chain_phases"].apply(lambda x: any(tactic["kill_chain_name"] == tactics_filter for tactic in x))]
+    
+    if technique_filter:
+        filtered_df = filtered_df[filtered_df["name"] == technique_filter]
+    
+    if group_filter:
+        filtered_df = filtered_df[filtered_df["x_mitre_groups"].apply(lambda x: group_filter in x)]
+    
+    return filtered_df
 
 def main():
-    st.title("MITRE ATT&CK Navigator - Enterprise")
-
     data = load_data()
-    software, tactics, techniques, groups, data_sources, initial_access = process_data(data)
-
-    software_filter = st.sidebar.selectbox("Filter by Software:", ["All"] + software)
-    tactic_filter = st.sidebar.selectbox("Filter by Tactic:", ["All"] + tactics)
-    technique_filter = st.sidebar.selectbox("Filter by Technique:", ["All"] + techniques)
-    group_filter = st.sidebar.selectbox("Filter by APT Group:", ["All"] + groups)
-    data_source_filter = st.sidebar.selectbox("Filter by Data Source:", ["All"] + data_sources)
-    initial_access_filter = st.sidebar.selectbox("Filter by Initial Access:", ["All"] + initial_access)
+    df, software, tactics, techniques, groups = process_data(data)
     
-    filtered_data = filter_data(data, software_filter, tactic_filter, technique_filter, group_filter,
-                                data_source_filter, initial_access_filter)
+    st.title("MITRE ATT&CK Navigator")
+    
+    st.sidebar.header("Filters")
+    software_filter = st.sidebar.selectbox("Software", [""] + software)
+    tactics_filter = st.sidebar.selectbox("Tactics", [""] + tactics)
+    technique_filter = st.sidebar.selectbox("Techniques", [""] + techniques)
+    group_filter = st.sidebar.selectbox("APT Groups", [""] + groups)
+    
+    filtered_df = filter_data(df, software_filter, tactics_filter, technique_filter, group_filter)
+    
+    st.sidebar.subheader("Selected Filters")
+    st.sidebar.write("Software:", software_filter)
+    st.sidebar.write("Tactics:", tactics_filter)
+    st.sidebar.write("Technique:", technique_filter)
+    st.sidebar.write("APT Group:", group_filter)
+    
+    if filtered_df.empty:
+        st.warning("No techniques found with the selected filters.")
+    else:
+        st.success(f"Displaying {filtered_df.shape[0]} techniques.")
+        st.dataframe(filtered_df)
 
-    st.markdown("### Techniques")
-    st.write(filtered_data)
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
